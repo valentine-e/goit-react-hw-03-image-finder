c8acd2d8d8863838a29e8117f48061d7a19859af
valentine-e <valia101112@gmail.com>
2023-10-27 16:13:07 +0300

    1 steps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    loading: false,
+    error: false,
+  };
+
+  hendleSubmit = evt => {
+    evt.preventDefault();
+    // зберегти значення пошуку (query)
+    // скинути page to 1
+    // очистити масив картинок
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => prevState.page + 1);
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.query !== prevState.query ||
+      this.state.page !== prevState.page
+    ) {
+      // hhtp запит і setState
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>Search form</form>
+        {this.state.images.length > 0 && <div>RGallery</div>}
+        <div>Loader</div>
+        <button onClick={this.handleLoadMore}>Load more</button>
+      </div>
+    );
+  }
+}
---
+++ src/components/api.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12'
